26ce1cb4e31382c5956fd6fff553d8ba3ef77129
Austin Landis <austinlandiscode@gmail.com>
2021-02-28 11:05:04 -0800

    delete UI logic

--- src/main.js
+++ src/main.js
@@ -5,22 +5,3 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-  $("#planetVariables").submit(function() {
-    event.preventDefault();
-    let age = parseInt($('#userAge').val());
-    let sex = $('#userSex').val();
-    let continent = $('#userContinent').val();
-    let planet = $('#userPlanet').val();
-
-    let ageCalculator = new Calculator(age,sex,continent,planet);
-  
-  ageCalculator.CheckYears(this.planetYears);
-  $('#output').text(`You are ${age} on earth, but will be ${this.planetYears} on ${planet}`);
-
-  let yearsCalculator = new Calculator(age,sex,continent,planet);
-
-  yearsCalculator.CheckYearsLeft(this.planetYearsLeft);
-  $('#output2').text(`You are ${age} on earth, and ${this.planetYears} on ${planet}. But you have ${this.planetYearsLeft} on ${planet}`);
-  });
-});
